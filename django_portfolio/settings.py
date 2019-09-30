edd8f2f2dec569fc9d3697c50087fcc863a431af
YuhangXiong <yxiong4@ualberta.ca>
2019-09-30 12:11:52 -0600

    change the database to postgres and add the blog model

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'project.apps.ProjectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Immi2015wang',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
