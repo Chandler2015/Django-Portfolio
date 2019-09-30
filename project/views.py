ad8e62d528673b8063dbc058464e6a070b4df6ef
YuhangXiong <yxiong4@ualberta.ca>
2019-09-30 13:26:39 -0600

    add the projects to the homepage

--- project/views.py
+++ project/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Project
+
 
 def home(request):
-    return render(request, 'project/home.html')
+    projects = Project.objects
+    return render(request, 'project/home.html', {'projects': projects})
