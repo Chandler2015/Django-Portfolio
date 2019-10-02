d241c2a70e5ea00196c8572d236c1a454e43af7a
YuhangXiong <yxiong4@ualberta.ca>
2019-10-01 21:42:06 -0600

    add the is published field to blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Blog
 
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'summary', 'pub_date_pretty', 'is_published']
+
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,13 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    pub_date = models.DateTimeField(null=True)
-    body = models.TextField(null=True)
-    image = models.ImageField(upload_to='images/', null=True)
+    pub_date = models.DateTimeField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    is_published = models.BooleanField(default=True)
 
     def summary(self):
-        return self.body[:100]
+        return self.body[:50]
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
